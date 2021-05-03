b024613e63669bef368cc8a910d82ee54408b9b6
masterdev <masterdev@borntobein.com>
2021-05-03 11:14:52 +0200

    ajout d'un réaction role pour les CGV + Annonce système

--- app.js
+++ app.js
@@ -1,23 +1,94 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-require('dotenv-flow').config();
+/** SECURE ENV **/
+require("dotenv").config();
 
-const config = {
-    token: process.env.TOKEN,
-    owner: process.env.OWNER,
-    prefix: process.env.PREFIX
-};
 
+const discord = require("discord.js");
+const client = new discord.Client({
+    partials: ["MESSAGE", "REACTION"]
+});
+const PREFIX = "!";
+
+const webhookClient = new discord.WebhookClient(
+    process.env.WEBHOOK_ID,
+    process.env.WEBHOOK_TOKEN,
+)
+
+/** BOT ACTION **/
 client.on('ready', () => {
     console.log(`[+] ${client.user.tag} est connecté avec succès !`);
     client.user.setActivity("Online");
-    client.user.setActivity("En cours de développement")
+    client.user.setStatus("online");
+    client.user.setActivity("En cours de développement");
 });
 
-client.on('message', msg => {
-    if (msg.content.toLowerCase() === 'ping') {
-        msg.reply('Arrêtez de Pong!');
+/** REACTION ROLES **/
+client.on('messageReactionAdd', (reaction, user) => {
+    const { name } = reaction.emoji;
+    const member = reaction.message.guild.members.cache.get(user.id);
+    if (reaction.message.id === process.env.CGU) {
+        switch (name) {
+            case '✅':
+                member.roles.add(process.env.MEMBER)
+                break;
+            case '❌':
+                member.roles.add(process.env.SOLDIER)
+                break;
+        }
+    }
+});
+client.on('messageReactionRemove', (reaction, user) => {
+    const { name } = reaction.emoji;
+    const member = reaction.message.guild.members.cache.get(user.id);
+    if (reaction.message.id === process.env.CGU) {
+        switch (name) {
+            case '✅':
+                member.roles.remove(process.env.MEMBER)
+                break;
+            case '❌':
+                member.roles.remove(process.env.SOLDIER)
+                break;
+        }
     }
 });
 
-client.login(config.token);
+client.on('guildMemberAdd', member => {
+    console.log('Un nouveau membre');
+    member.guild.channels.cache.find(channel => channel.id === process.env.WELCOME_CHAN)
+    send(member.displayName + 'nous a rejoins')
+});
+
+client.on('guildMemberRemove', member => {
+    console.log('Un membre nous quitté');
+    member.guild.channels.cache.find(channel => channel.id === process.env.WELCOME_CHAN).send(member.displayName + 'est partie')
+});
+
+client.on('message', msg => {
+    const embed = new discord.MessageEmbed()
+        .setTitle('OYE OYE')
+        .setColor('GREEN')
+        .setDescription('Ton avis nous intéresse!')
+        .setThumbnail('')
+
+    if (msg.author.bot);
+    let CMD_NAME = [];
+    if (msg.content.startsWith(PREFIX)) {
+        CMD_NAME = msg.content
+            .trim()
+            .substring(PREFIX.length).toUpperCase()
+            .split(" ");
+        console.log(CMD_NAME);
+    }
+    if (CMD_NAME[0] === 'ANNOUNCE') {
+        webhookClient.send(embed);
+    }
+    if (CMD_NAME[0] === 'PING') {
+        webhookClient.send(embed);
+    }
+    if (CMD_NAME[0] === 'CTF') {
+        webhookClient.send(embed);
+    }
+
+});
+
+/** TOKEN SECTION  **/
+client.login(process.env.TOKEN);
